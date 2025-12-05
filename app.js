d67070d72b8a505aa8acf2ea50c1ceacee780196
nasblack4-lgtm <nasblack4@gmail.com>
2025-12-05 03:07:43 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-// Trigger crash after 5 seconds
 setTimeout(() => {
   document.body.classList.add("crash");
 
-  // Spawn glitch fragments
   const container = document.getElementById("fragments");
   for (let i = 0; i < 50; i++) {
     const span = document.createElement("span");
     span.className = "fragment";
     span.textContent = "TCT";
-    // Random scatter directions
     const x = (Math.random() - 0.5) * 800 + "px";
     const y = (Math.random() - 0.5) * 600 + "px";
     span.style.setProperty("--x", x);
